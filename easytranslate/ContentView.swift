87f546822bee49e496d52f712d50637cba22a3c1
dorin <dorin.rogov@gmail.com>
2025-11-15 14:05:05 -0500

    Initial Commit

---
+++ easytranslate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  easytranslate
+//
+//  Created by Dorin Rogov on 2025-11-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ easytranslate/easytranslateApp.swift
@@ -0,0 +1,17 @@
+//
+//  easytranslateApp.swift
+//  easytranslate
+//
+//  Created by Dorin Rogov on 2025-11-15.
+//
+
+import SwiftUI
+
+@main
+struct easytranslateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
